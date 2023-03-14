6b19230b61feb627b83b03180da6815204c6690e
Mehek09 <107671119+Mehek09@users.noreply.github.com>
2023-03-14 11:59:36 +0530

    Add files via upload

---
+++ QueueInterface1/Ttester.java
@@ -0,0 +1,10 @@
+package QueueInterface1;
+
+public class Ttester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ QueueInterface2/Tester.java
@@ -0,0 +1,10 @@
+package QueueInterface2;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
